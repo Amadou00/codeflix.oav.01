0246f43d8a2151bd826f5b3715bd0a907605c24f
Keitaraymond@ymail.ci*om <King1997>
2020-10-27 14:46:20 +0100

    Envoit retarder par un entretient d'alternance

--- oav.02.js
+++ oav.02.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 function translate(fichier){
-    // /\[.+[A-Z]+\]/g
-    //let titles = file.match(/((#.+[A-Z]))/g);
+
     const regexEqual = /(=)/g;
     let result = "{\n";
     let file = fs.readFileSync(fichier, 'utf8');
     const splittedContent = file.split(/\r?\n/);
     splittedContent.forEach(function (line, i) {
-        if (!line.match(/^#/) && line.length != 0 && i < splittedContent.length-2){
+        if (line.match(/\[(.[A-Z].)\]/gm) && line.length != 0 && i < splittedContent.length-2){
             line = '    "' + line + '",'; 
             result += line + "\n";
         }
@@ -19,9 +18,9 @@ function translate(fichier){
     let changeEqual = result.replace(regexEqual, '" : "');
     changeEqual += "}";
     
-    fs.writeFileSync("text.json", changeEqual);
+    fs.writeFileSync("php.20181003133700.json", changeEqual);
     //return titles;
     console.log(changeEqual);
 }
 
-console.log(translate("env.txt"));
+console.log(translate("php.ini"));
